1f1dcfafa89224f09ae6fc2aae531cf9778f9289
AbhayRao2k <abhayrao2k@gmail.com>
2024-02-23 20:46:17 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,7 @@
+// Setup and start animation!
+
+var typed = new Typed('#element', {
+    strings: ['Full Stack Engineer', 'Video Editor.'],
+    typeSpeed: 50,
+    loop: true // To keep the animation loop indefinitely
+});
